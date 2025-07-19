d8a5a34318da0f6ef8e3d6e4960a5b34eae7c89c
valeennmendez <mendezvalentin433@gmail.com>
2025-07-19 01:47:24 -0300

    Pruebas

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use("/api/movie", movieRouths);
 
 //if (process.env.NODE_ENV === "production") {
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-app.get("/.*/", (req, res) => {
+app.get(/^\/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 //}
