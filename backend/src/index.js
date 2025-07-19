258518c9cc8aeebada3143470a31b87bfdec4a60
valeennmendez <mendezvalentin433@gmail.com>
2025-07-19 01:37:14 -0300

    Pruebas

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use("/api/movie", movieRouths);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-  });
-}
+//if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
+//}
 
 app.listen(5001, () => {
   console.log("Servidor corriendo en el puerto 5001");
