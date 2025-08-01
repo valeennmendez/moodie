58c428d4242050e41c44972e3f61b9408a86d565
valeennmendez <mendezvalentin433@gmail.com>
2025-08-01 16:05:38 -0300

    Cambio en axios.js donde se evalua la variable de entorno VITE_ENV

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
+const DEV = import.meta.env.VITE_ENV;
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api/",
+  baseURL: `${
+    DEV === "development"
+      ? "http://localhost:5001/api/"
+      : "https://moodie.onrender.com/api/"
+  }`,
   withCredentials: true,
 });
